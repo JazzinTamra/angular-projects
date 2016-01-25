5c6e3622c6038969dc472aa981b5d5eeb215eced
Tamra Fenstermaker <fenstermaker505@gmail.com>
2016-01-25 11:48:32 -0700

    Angular is working!!!

--- assets/calculator.js
+++ assets/calculator.js
@@ -1,13 +1,22 @@
 /* JavaScript Document */
 
-(function() { //function within () allows this to live in this file with other javascripts
+(function(){
+    var app = angular.module("myCalculator",[]);
 
-    var app = angular.module('myCalculator',[]); //declaring the varialbe app to be myCalculator
-    app.controller('CalculatorController',['$scope',function($scope){ //defining controller(name),service is $scope, putting $scope in the function allows
-        $scope.lumen_options = [375, 600, 900, 1125, 1600]; //the list in the dropdown menu
-        $scope.current_lumens = 600; //set the options to begin with this
-
+    app.controller("CalculatorController",["$scope",function($scope){
+        $scope.lumen_options = [375, 600, 900, 1125, 1600];
+        $scope.current_lumens = 600;
+        $scope.current_cost = 13;
+        $scope.current_hours = 4;
+        $scope.current_days = 365;
     }])
+
 })();
+
+//function within () allows this to live in this file with other javascripts
+//declaring the variable app to be myCalculator
+//defining controller(name),service is $scope, putting $scope in the function allows
+//the list in the drop down menu
+//set the options to begin with this
 
 
